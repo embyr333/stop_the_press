43e112bbca174095544db0a55c501d0df38b4ae3
embyr333 <emb333@gmail.com>
2025-07-07 14:15:52 +0100

    2nd commit (Snapshot2) Added a GUI*, though have only 'line removal' functionality so far

--- stop_the_press.py
+++ stop_the_press.py
@@ -1,39 +1,49 @@
 '''
-StopThePress - Version1, Snapshot1
+StopThePress - Snapshot2 
+Added a GUI*, though have only 'line removal' functionality so far.
+(*Adapting my "modstr" code to help remember how to set up a tkinter GUI)
 
-These functions can be used to comment-out or remove
-lines of code ending in a specified substring,
-e.g. '# temp check' marking temporary print statements
-for checking intermediate value in algorithms.  
-Caveat: Splits any internally-coded string linebreaks, 
-so best not to use on code includng these.
-
-TODO Make a version for GUI input & output.
+TODO next: add the 'comment out' alternative functionality.
 '''
 
-def remove_lines_with_suffix(text: str, suffix: str):
+def remove_lines_with_suffix(text: str, suffix: str): 
+    if text == '': 
+        output_field.insert("end", 'Please enter the code you want to process')
+        return # (not essential, just prevents pointless calls)
     lines = text.splitlines()
     for line in lines:
         if not line.endswith(suffix):
-            print(line)
+            output_field.insert("end", line + '\n')
+
+def submit_click(): # actions for Submit button in GUI below
+    output_field.delete('1.0', END) # clear any existing output
+    remove_lines_with_suffix(input_field.get("1.0",'end').rstrip(), input_field_2.get("1.0",'end').rstrip()) 
 
 
-def comment_out_lines_with_suffix(text: str, suffix: str):
-    lines = text.splitlines()
-    for i in range(len(lines)):
-        if lines[i].endswith(suffix):
-            lines[i] = '# ' + lines[i]
-        print(lines[i])
+# GUI...
 
+from tkinter import *
+from tkinter.scrolledtext import ScrolledText
+root_widget = Tk()
+root_widget.title("stop_the_press removes or comments out (adds # before) lines endng in a specifed suffix")
+root_widget.geometry("1010x740") # provisional width, height GUI
 
-# Replace contents of this placeholder/example  
-# string with text to be processed
-test = '''
-line1
-line2 # temp check
-line3# temp check
-line4
-'''
+Label(root_widget, text = 'Enter code (top box) and suffix (small middle box))').grid(row=0, column=0) 
+# (1st row of grid, so row index is 0)
 
-remove_lines_with_suffix(test, '# temp check')
-comment_out_lines_with_suffix(test, '# temp check')
+input_field = ScrolledText(root_widget, width = 120, fg = 'blue', font=("Courier", 10), height=20) # input textbox (on 2nd row)
+input_field.grid(row=1, column=0, padx=15) 
+
+output = StringVar()
+
+input_field_2 = Text(root_widget, width = 20, fg = 'blue', font=("Courier", 10), height=1)
+input_field_2.grid(row=2, column=0, padx=15) 
+
+# 'Submit' button on 4th row grid (sets output text in 5th row)
+Button(root_widget, text = 'Submit', command = submit_click, bg='#C8C8C8').grid(row=3, column=0)
+
+output_field = ScrolledText(root_widget, width = 120, fg = 'blue', font=("Courier", 10), height=20) # output textbox 
+output_field.grid(row=4, column=0) 
+
+root_widget.mainloop()
+
