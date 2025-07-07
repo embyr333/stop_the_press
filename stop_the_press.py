deef85a0cb09902fec25edb4da57baba74bc3193
embyr333 <emb333@gmail.com>
2025-07-07 14:19:04 +0100

    4th commit (Snapshot4) Refactored a bit to reduce redundant code

--- stop_the_press.py
+++ stop_the_press.py
@@ -1,36 +1,27 @@
 '''
-StopThePress - Snapshot3
-Added the 'comment out' alternative functionality.
+StopThePress - Snapshot4
+Refactored a bit to reduce redundant code.
 
-TODO: Consider whether the code can be refactored to reduce redundancies.
+TODO: Consider whether the code can be refacored further.
 '''
 
-def comment_out_lines_with_suffix(text: str, suffix: str):
+def process(button: str):
+    output_field.delete('1.0', END) # clear any existing output
+    text = input_field.get("1.0",'end').rstrip()
+    suffix = input_field_2.get("1.0",'end').rstrip()
     if not text or not suffix: 
         output_field.insert("end", 'Please enter your code and suffix')
-        return         
+        return   
     lines = text.splitlines()
-    for i in range(len(lines)):
-        if lines[i].endswith(suffix):
-            lines[i] = '# ' + lines[i]
-        output_field.insert("end", lines[i] + '\n')
-
-def remove_lines_with_suffix(text: str, suffix: str): 
-    if not text or not suffix: 
-        output_field.insert("end", 'Please enter your code and suffix')
-        return 
-    lines = text.splitlines()
-    for line in lines:
-        if not line.endswith(suffix):
-            output_field.insert("end", line + '\n')
-
-def comment_out_click(): # actions for Comment Out button in GUI below
-    output_field.delete('1.0', END) # clear any existing output
-    comment_out_lines_with_suffix(input_field.get("1.0",'end').rstrip(), input_field_2.get("1.0",'end').rstrip()) 
-
-def remove_click(): # actions for Remove button in GUI below
-    output_field.delete('1.0', END) # clear any existing output
-    remove_lines_with_suffix(input_field.get("1.0",'end').rstrip(), input_field_2.get("1.0",'end').rstrip()) 
+    if button == 'c':
+        for i in range(len(lines)):
+            if lines[i].endswith(suffix):
+                lines[i] = '# ' + lines[i]
+            output_field.insert("end", lines[i] + '\n')
+    else: # (button == 'r')
+        for line in lines:
+            if not line.endswith(suffix):
+                output_field.insert("end", line + '\n')
 
 # GUI...
 
@@ -52,9 +43,9 @@ input_field_2 = Text(root_widget, width = 20, fg = 'blue', font=("Courier", 10),
 input_field_2.grid(row=2, column=0, padx=15) 
 
 # Buttons on 4th & 5th rows grid (set output text in 6th row)
-comment_out_button = Button(root_widget, text = 'Comment Out', command = comment_out_click, bg='#C8C8C8')
+comment_out_button = Button(root_widget, text = 'Comment Out', command = lambda: process('c'), bg='#C8C8C8')
 comment_out_button.grid(row=3, column=0)
-remove_button = Button(root_widget, text = 'Remove', command = remove_click, bg='#C8C8C8')
+remove_button = Button(root_widget, text = 'Remove', command = lambda: process('r'), bg='#C8C8C8')
 remove_button.grid(row=4, column=0)
 
 output_field = ScrolledText(root_widget, width = 120, fg = 'blue', font=("Courier", 10), height=20) # output textbox 
